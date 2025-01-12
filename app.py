7a7fa4a1b2e01a2024b4969c364c26d45568c3c3
Sitam <sitammeur@gmail.com>
2025-01-12 16:38:34 +0530

    re write up for modernbert adoption

--- app.py
+++ app.py
@@ -1,62 +1,52 @@
 # Necessary imports
 import gradio as gr
-from transformers import pipeline
-
-
-# Load the zero-shot classification model
-classifier = pipeline(
-    "zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
-)
-
-
-# Function to perform zero-shot classification
-def ZeroShotTextClassification(text_input, candidate_labels):
-    """
-    Performs zero-shot classification on the given text input using the provided candidate labels.
-
-    Args:
-      text_input (str): The input text to classify.
-      candidate_labels (str): A comma-separated string of candidate labels.
-
-    Returns:
-      dict: A dictionary containing the predicted labels as keys and their corresponding scores as values.
-    """
-
-    # Split the candidate labels
-    labels = [label.strip(" ") for label in candidate_labels.split(",")]
-
-    # Perform zero-shot classification
-    prediction = classifier(text_input, labels)
-
-    return {
-        prediction["labels"][i]: prediction["scores"][i]
-        for i in range(len(prediction["labels"]))
-    }
+from classytext.classifier.predict import ZeroShotTextClassification
 
 
 # Examples to display in the interface
 examples = [
-    ["I love to play the guitar", "music, artist, food, travel"],
-    ["I am a software engineer at Google", "technology, engineering, art, science"],
-    ["I am a professional basketball player", "sports, athlete, chef, politics"],
+    [
+        "I have a problem with my iPhone that needs to be resolved asap!",
+        "urgent, not urgent, phone, tablet, computer",
+        False,
+    ],
+    [
+        "Angela Merkel is a politician in Germany and leader of the CDU",
+        "politics, economy, entertainment, environment",
+        False,
+    ],
+    [
+        "Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!",
+        "refund, legal, faq",
+        True,
+    ],
 ]
 
 # Title and description and article for the interface
 title = "Zero Shot Text Classification"
-description = "Classify text using zero-shot classification with DeBERTa-v3-large-zeroshot model! Provide a text input and a list of candidate labels separated by commas. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://arxiv.org/pdf/2312.17543.pdf' target='_blank'>Building Efficient Universal Classifiers with Natural Language Inference</a> | <a href='https://huggingface.co/MoritzLaurer/deberta-v3-large-zeroshot-v2.0' target='_blank'>Model Page</a></p>"
+description = "Classify text using zero-shot classification with ModernBERT-large zeroshot model! Provide a text input and a list of candidate labels separated by commas. Read more at the links below."
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2412.13663' target='_blank'>Smarter, Better, Faster, Longer: A Modern Bidirectional Encoder for Fast, Memory Efficient, and Long Context Finetuning and Inference</a> | <a href='https://huggingface.co/MoritzLaurer/ModernBERT-large-zeroshot-v2.0' target='_blank'>Model Page</a></p>"
 
 
 # Launch the interface
 demo = gr.Interface(
     fn=ZeroShotTextClassification,
-    inputs=[gr.Textbox(label="Input"), gr.Textbox(label="Candidate Labels")],
+    inputs=[
+        gr.Textbox(label="Input", placeholder="Enter text to classify"),
+        gr.Textbox(
+            label="Candidate Labels",
+            placeholder="Enter candidate labels separated by commas",
+        ),
+        gr.Checkbox(label="Multi-Label", value=False),
+    ],
     outputs=gr.Label(label="Classification"),
     title=title,
     description=description,
     article=article,
     examples=examples,
+    cache_examples=True,
+    cache_mode="lazy",
     theme="Soft",
-    allow_flagging="never",
+    flagging_mode="never",
 )
 demo.launch(debug=False)
---
+++ classytext/classifier/predict.py
@@ -0,0 +1,61 @@
+# Necessary imports
+import sys
+from typing import Dict
+from classytext.logger import logging
+from classytext.exception import CustomExceptionHandling
+from transformers import pipeline
+import gradio as gr
+
+
+# Load the zero-shot classification model
+classifier = pipeline(
+    "zero-shot-classification",
+    model="MoritzLaurer/ModernBERT-large-zeroshot-v2.0",
+)
+
+
+def ZeroShotTextClassification(
+    text_input: str, candidate_labels: str, multi_label: bool
+) -> Dict[str, float]:
+    """
+    Performs zero-shot classification on the given text input.
+
+    Args:
+        - text_input: The input text to classify.
+        - candidate_labels: A comma-separated string of candidate labels.
+        - multi_label: A boolean indicating whether to allow the model to choose multiple classes.
+
+    Returns:
+        Dictionary containing label-score pairs.
+    """
+    try:
+        # Check if the input and candidate labels are valid
+        if not text_input or not candidate_labels:
+            gr.Warning("Please provide valid input and candidate labels")
+
+        # Split and clean the candidate labels
+        labels = [label.strip() for label in candidate_labels.split(",")]
+
+        # Log the classification attempt
+        logging.info(f"Attempting classification with {len(labels)} labels")
+
+        # Perform zero-shot classification
+        hypothesis_template = "This text is about {}"
+        prediction = classifier(
+            text_input,
+            labels,
+            hypothesis_template=hypothesis_template,
+            multi_label=multi_label,
+        )
+
+        # Return the classification results
+        logging.info("Classification completed successfully")
+        return {
+            prediction["labels"][i]: prediction["scores"][i]
+            for i in range(len(prediction["labels"]))
+        }
+
+    # Handle exceptions that may occur during the process
+    except Exception as e:
+        # Custom exception handling
+        raise CustomExceptionHandling(e, sys) from e
